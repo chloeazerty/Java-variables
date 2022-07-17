cf77e334279c8d5f59b5ac6feae7c6b34d18f493
chloe azerty <chloe.rotaru@yahoo.com>
2022-07-17 17:52:54 +0200

    variables

---
+++ Movie.java
@@ -0,0 +1,19 @@
+public class Movie {
+    public static void main(String[] args) {
+        String movieName;
+        boolean isSeen;
+        int releaseYear;
+        float movieNote;
+
+        movieName = "Indiana Jones and the Last Crusade";
+        isSeen = true;
+        releaseYear = 1989;
+        movieNote = 8.2F;
+
+        System.out.println(movieName);
+        System.out.println(isSeen);
+        System.out.println(releaseYear);
+        System.out.println(movieNote);
+
+    }
+}
